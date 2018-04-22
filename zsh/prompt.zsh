9b2f721cb544ee6eecc97041d62f02d7c9458316
Arnold Almeida <arnold@floatingpoints.com.au>
2018-04-22 18:17:59 +0100

    Drop unused function

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -7,34 +7,29 @@ else
   git="/usr/bin/git"
 fi
 
-_git_branch() {
-  echo $($git symbolic-ref HEAD 2>/dev/null | awk -F/ {'print $NF'})
-}
-
-_git_dirty() {
-  if git rev-parse --git-dir > /dev/null 2>&1; then
+git_dirty() {
+  if $git rev-parse --git-dir > /dev/null 2>&1; then
     if [[ $(git diff --stat) != '' ]]; then
-      echo "on %{$fg_bold[red]%}$(_git_prompt_info)%{$reset_color%}"
+      echo "on %{$fg_bold[red]%}$(git_prompt_info)%{$reset_color%}"
     else
-      echo "on %{$fg_bold[green]%}$(_git_prompt_info)%{$reset_color%}"
+      echo "on %{$fg_bold[green]%}$(git_prompt_info)%{$reset_color%}"
     fi
   else
     :
   fi
 }
 
-_git_prompt_info () {
+git_prompt_info () {
   ref=$($git symbolic-ref HEAD 2>/dev/null) || return
-# echo "(%{\e[0;33m%}${ref#refs/heads/}%{\e[0m%})"
- echo "${ref#refs/heads/}"
+  echo "${ref#refs/heads/}"
 }
 
-_unpushed () {
+unpushed () {
   $git cherry -v @{upstream} 2>/dev/null
 }
 
-_requires_push () {
-  if [[ $(_unpushed) == "" ]]
+requires_push () {
+  if [[ $(unpushed) == "" ]]
   then
     echo " "
   else
@@ -42,7 +37,7 @@ _requires_push () {
   fi
 }
 
-_current_directory() {
+current_directory() {
   echo "%{$fg_bold[cyan]%}${PWD/#$HOME/~}%{$reset_color%}"
 }
 
@@ -67,5 +62,5 @@ chpwd() {
 
 # Set the prompt
 set_prompt () {
-  export PROMPT=$'\nin $(_current_directory) $(_git_dirty)$(_requires_push)\n› '
+  export PROMPT=$'\nin $(current_directory) $(git_dirty)$(requires_push)\n› '
 }
